5e78cc2f5b1065ef4d8e082e9d961c4e0e27a077
ChristineXYS <flychristine@gmail.com>
2015-10-16 17:28:21 +0800

    As core plugin's id changed from "com.streethawk.plugin" to "com.streethawk.core", customer's js need to modify accordingly.

--- www/js/common.js
+++ www/js/common.js
@@ -3,20 +3,20 @@ document.addEventListener("resume", shResume, false);
 
 
 function shFailure(){
-var sh = cordova.require("com.streethawk.plugin.Streethawk");
+var sh = cordova.require("com.streethawk.core.Streethawk");
 	sh.shRegisterViewCallback(launchAppPage,shFailure);
 }
 function launchAppPage(result){
-		var sh = cordova.require("com.streethawk.plugin.Streethawk");
+		var sh = cordova.require("com.streethawk.core.Streethawk");
 		sh.shRegisterViewCallback(launchAppPage,shFailure);
 		window.location = result;
         }
 function shOnDeviceReady(){
-	var sh = cordova.require("com.streethawk.plugin.Streethawk");
+	var sh = cordova.require("com.streethawk.core.Streethawk");
 	sh.shRegisterViewCallback(launchAppPage,shFailure);
 }
 function shResume(){
-	var sh = cordova.require("com.streethawk.plugin.Streethawk");
+	var sh = cordova.require("com.streethawk.core.Streethawk");
 	sh.shGetViewName(function(result){
 		if(null!=result){
 			launchAppPage(result);
